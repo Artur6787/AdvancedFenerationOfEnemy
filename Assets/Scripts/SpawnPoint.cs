04d132a1223c46a2ea20bacb8f00f76d3b0c8d94
Artur6787 <starx152@gmail.com>
2024-12-26 15:39:22 +0300

    Update SpawnPoint.cs

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -1,23 +1,3 @@
-//using UnityEngine;
-
-//public class SpawnPoint : MonoBehaviour
-//{
-//    private Enemy _template;
-//    private Target _target;
-
-//    public void Spawn()
-//    {
-//        if (_target != null && _template != null)
-//        {
-//            Enemy enemy = Instantiate(_template, transform.position, Quaternion.identity);
-//            enemy.SetTarget(_target);
-//        }
-//    }
-
-//    public void SetTemplate(Enemy template) => _template = template;
-
-//    public void SetTarget(Target target) => _target = target;
-//}
 using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
