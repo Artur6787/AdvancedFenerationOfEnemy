10b7a7ac03514464079d5b86269710074e06c757
Artur6787 <starx152@gmail.com>
2024-12-26 15:37:01 +0300

    init

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -1,20 +1,39 @@
+//using UnityEngine;
+
+//public class SpawnPoint : MonoBehaviour
+//{
+//    private Enemy _template;
+//    private Target _target;
+
+//    public void Spawn()
+//    {
+//        if (_target != null && _template != null)
+//        {
+//            Enemy enemy = Instantiate(_template, transform.position, Quaternion.identity);
+//            enemy.SetTarget(_target);
+//        }
+//    }
+
+//    public void SetTemplate(Enemy template) => _template = template;
+
+//    public void SetTarget(Target target) => _target = target;
+//}
 using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
-    private Enemy _template;
-    private Target _target;
+    [SerializeField] private Enemy template;
+    [SerializeField] private Target target;
 
     public void Spawn()
     {
-        if (_target != null && _template != null)
+        if (target != null && template != null)
         {
-            Enemy enemy = Instantiate(_template, transform.position, Quaternion.identity);
-            enemy.SetTarget(_target);
+            Enemy enemy = Instantiate(template, transform.position, Quaternion.identity);
+            enemy.SetTarget(target);
         }
     }
 
-    public void SetTemplate(Enemy template) => _template = template;
-
-    public void SetTarget(Target target) => _target = target;
+    public void SetTemplate(Enemy newTemplate) => template = newTemplate;
+    public void SetTarget(Target newTarget) => target = newTarget;
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,23 +1,56 @@
+//using System.Collections;
+//using UnityEngine;
+
+//public class Spawner : MonoBehaviour
+//{
+//    [SerializeField] private Enemy[] _templates;
+//    [SerializeField] private Target[] _targets;
+//    [SerializeField] private float _spawnTime;
+
+//    private SpawnPoint[] _points;
+//    private int _minValue = 0;
+//    private bool _isWork = true;
+
+//    private void Awake()
+//    {
+//        _points = gameObject.GetComponentsInChildren<SpawnPoint>();
+
+//        for (int i = 0; i < _targets.Length && i < _points.Length; i++)
+//        {
+//            _points[i].SetTemplate(_templates[i]);
+//            _points[i].SetTarget(_targets[i]);
+//        }
+//    }
+
+//    private void Start()
+//    {
+//        StartCoroutine(Spawn());
+//    }
+
+//    private IEnumerator Spawn()
+//    {
+//        WaitForSeconds time = new(_spawnTime);
+
+//        while (_isWork)
+//        {
+//            _points[Random.Range(_minValue, _points.Length)].Spawn();
+
+//            yield return time;
+//        }
+//    }
+//}
 using System.Collections;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private Enemy[] _templates;
-    [SerializeField] private Target[] _targets;
-    [SerializeField] private float _spawnTime;
-
-    private SpawnPoint[] _points;
-
+    [SerializeField] private float spawnTime;
+    private SpawnPoint[] points;
+    private bool isWorking = true;
+    private int minValue = 0;
     private void Awake()
     {
-        _points = gameObject.GetComponentsInChildren<SpawnPoint>();
-
-        for (int i = 0; i < _targets.Length && i < _points.Length; i++)
-        {
-            _points[i].SetTemplate(_templates[i]);
-            _points[i].SetTarget(_targets[i]);
-        }
+        points = GetComponentsInChildren<SpawnPoint>();
     }
 
     private void Start()
@@ -27,11 +60,14 @@ public class Spawner : MonoBehaviour
 
     private IEnumerator Spawn()
     {
-        WaitForSeconds time = new(_spawnTime);
+        WaitForSeconds time = new WaitForSeconds(spawnTime);
 
-        while (true)
+        while (isWorking)
         {
-            _points[Random.Range(0, _points.Length)].Spawn();
+            if (points.Length > minValue)
+            {
+                points[Random.Range(minValue, points.Length)].Spawn();
+            }
 
             yield return time;
         }
